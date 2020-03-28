85d748e8a550f30b38fce2f2fbe8d75d9d0d7837
Luana Martins <luana.cin@gmail.com>
2020-03-28 10:27:05 -0300

    Add print and remove methods for LinkedList

--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -16,6 +16,26 @@ class MyLinkedList:
         else:
             self.head = Node(value)
 
+    def print(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
+    def remove(self, value):
+        current = self.head
+        prev = None
+        while current:
+            if current.data == value:
+                if prev:
+                    prev.next = current.next
+                else:
+                    self.head = current.next
+                return True
+            prev = current
+            current = current.next
+        return False
+
 
 myList = MyLinkedList()
 myList.add(2)
@@ -23,4 +43,4 @@ myList.add(3)
 myList.add(4)
 myList.add(3)
 myList.add(7)
-
+myList.print()
