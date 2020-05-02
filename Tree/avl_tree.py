baa58ec202b21266bac03efd9e7355c4ba283a26
Luana Martins <luana.cin@gmail.com>
2020-05-02 17:00:26 -0300

    Update avl tree and b tree

--- Tree/avl_tree.py
+++ Tree/avl_tree.py
@@ -1,1 +1,21 @@
+class AVLTree:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
+    def add(self, value):
+        if (self.data > value):
+            if (self.left == None):
+                self.left = Tree(value)
+            else:
+                self.left.add(value)
+        
+        elif (self.data < value):
+            if (self.right == None):
+                self.right = Tree(value)
+            else:
+                self.right.add(value)
+
+    def remove(self, value):
+        print('todo')
--- Tree/b_tree.py
+++ Tree/b_tree.py
@@ -1,1 +1,7 @@
-
+class BNode:
+    def __init__(self, contents=[], children=[]):
+        self.contents = contents
+        self.children = children
+    
+    def add(self, data):
+        self.contents.insert(data)
